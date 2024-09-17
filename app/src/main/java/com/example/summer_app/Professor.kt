1b1e87748214ccfc4a1029a84955ba629285c5cc
sheepyd3v <bizarr.development@gmail.com>
2024-09-17 00:19:32 -0700

    change schedule to a map from a course code to list of schedules

--- app/src/main/java/com/example/summer_app/Professor.kt
+++ app/src/main/java/com/example/summer_app/Professor.kt
@@ -3,7 +3,7 @@ package com.example.summer_app
 
 data class Professor(
     val name: String = "unknown",
-    val schedule: List<String> = listOf(),
+    val all_schedules: Map<String, List<String>> = mapOf(),
     val num_ratings: Int = 0,
     val difficulty: Double = 0.0,
     val overall_rating: Double = 0.0,
--- app/src/main/python/ProfessorsFetcher.py
+++ app/src/main/python/ProfessorsFetcher.py
@@ -34,20 +34,22 @@ def get_soup(url):
 def build_professor_table(rows, full_coruse_code):
 	professor_table = {}
 
-	for row in rows:
+	for i in range(len(rows)):
+		row = rows[i]
 		columns = row.find_all('td')
 
 		# Check if the row has enough columns and matches the full_coruse_code
 		if len(columns) > 7 and columns[1].text == full_coruse_code:
 			professor_name = columns[7].text
-			schedule = f"{get_days(columns[5].text)} - {columns[6].text}/{columns[8].text}"
+			class_code = columns[0].text
+			schedules = build_schedules(rows, i)
 
 			if professor_name in professor_table:
-				professor_table[professor_name]["schedule"].append(schedule)  # Append schedule
+				professor_table[professor_name]["all_schedules"][class_code] = schedules
 			else:
 				professor_table[professor_name] = {
 					"name": convert_name(professor_name),
-					"schedule": [schedule],
+					"all_schedules": {class_code: schedules},
 					"num_ratings": 0,
 					"difficulty": 0.0,
 					"overall_rating": 0.0,
@@ -55,6 +57,32 @@ def build_professor_table(rows, full_coruse_code):
 				}
 
 	return professor_table
+
+def build_schedules(rows, start_row_i):
+	schedules = []
+
+	columns = rows[start_row_i].find_all('td')
+	schedule = build_schedule(columns = columns, days_col = 5, hours_col = 6, location_col = 8)
+	schedules.append(schedule)
+
+	following_row_i = start_row_i + 1
+	while following_row_i < len(rows):
+		columns_for_following_row = rows[following_row_i].find_all('td')
+		if len(columns_for_following_row) < 7:
+			schedule = build_schedule(columns = columns_for_following_row, days_col = 1, hours_col = 2, location_col = 4)
+			schedules.append(schedule)
+			following_row_i += 1
+		else:
+			break
+
+	return schedules
+
+def build_schedule(columns, days_col, hours_col, location_col):
+	schedule = f"{get_days(columns[days_col].text)} - {columns[hours_col].text}/{columns[location_col].text}"
+	if not "TBA" in schedule:
+		return schedule
+	else:
+		return "No schedule/ONLINE"
 
 def convert_name(name):
 	parts = name.split(',')
@@ -70,5 +98,6 @@ def get_days(input):
 	return input.replace("·", "")
 
 if __name__ == "__main__":
-	# print(get_professors_data(department_code = "MATH", coruse_code = "1A", term_code = "F2024"))
-	print(get_terms())
+	print(get_professors_data(department_code = "MATH", coruse_code = "1A", term_code = "F2024"))
+	# print(get_professors_data(department_code = "PHYS", coruse_code = "2A", term_code = "F2024"))
+	# print(get_terms())
