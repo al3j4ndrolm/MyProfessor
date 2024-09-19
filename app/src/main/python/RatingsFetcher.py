c4e485ead6ccfc423e38f928077b1a899859280a
al3j4ndrolm <lopezalejandro5b@gmail.com>
2024-09-18 20:22:56 -0700

    Modify backend for testing purpose (Fetching data from RMP)

--- app/src/main/python/RatingsFetcher.py
+++ app/src/main/python/RatingsFetcher.py
@@ -4,21 +4,21 @@ import time
 ######################## Exported to be used in kotlin ########################
 
 def get_ratings(professor_name):
-    professor_dict = {
-        "professor_name": professor_name,
-        "difficulty": 0.0,
-        "would_take_again": 0.0,
-        "overall_rating": 0.0,
-        "review_num": 1,
-    }
+	professor_dict = {
+		"professor_name": professor_name,
+		"difficulty": 0.0,
+		"would_take_again": 0.0,
+		"overall_rating": 0.0,
+		"review_num": 1,
+	}
 
-    print(f"RatingsFetcher: Received call get_ratings({professor_name}).")
-    
-    time.sleep(2)
+	print(f"RatingsFetcher: Received call get_ratings({professor_name}).")
+	
+	time.sleep(2)
 
-    print(f"RatingsFetcher: Returning ratings data of get_ratings({professor_name}) to client.")
+	print(f"RatingsFetcher: Returning ratings data of get_ratings({professor_name}) to client.")
 
-    return professor_dict
+	return professor_dict
 
 ######################## Exported to be used in kotlin ########################
 
